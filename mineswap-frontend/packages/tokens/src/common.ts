1b8b0391a0575acdbf97e88e38c1762b149157a1
hoanghoauit@gmail.com <Hoa@Le1992>
2022-10-21 13:32:09 +0700

    update link

--- mineswap-frontend/packages/tokens/src/common.ts
+++ mineswap-frontend/packages/tokens/src/common.ts
@@ -6,7 +6,7 @@ export const CAKE_MAINNET = new Token(
   18,
   'CAKE',
   'MineSwap Token',
-  'https://pancakeswap.finance/',
+  'https://mineswap.finance/',
 )
 
 export const CAKE_TESTNET = new Token(
@@ -15,7 +15,7 @@ export const CAKE_TESTNET = new Token(
   18,
   'CAKE',
   'MineSwap Token',
-  'https://pancakeswap.finance/',
+  'https://mineswap.finance/',
 )
 
 export const USDC_ETH = new Token(ChainId.ETHEREUM, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6, 'USDC', 'USD Coin')
