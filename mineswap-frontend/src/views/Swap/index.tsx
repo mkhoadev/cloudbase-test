56a8fe67064d3c785ef90ee9ef476d75602b9edd
Nhan Nguyen <ntnhanbk@gmail.com>
2023-05-19 14:45:58 +0700

    remove chart

--- mineswap-frontend/src/views/Swap/index.tsx
+++ mineswap-frontend/src/views/Swap/index.tsx
@@ -67,7 +67,7 @@ export default function Swap() {
   return (
     <Page removePadding={isChartExpanded} hideFooterOnDesktop={isChartExpanded}>
       <Flex width="100%" justifyContent="center" position="relative">
-        {!isMobile && isChartSupported && (
+        {/* {!isMobile && isChartSupported && (
           <PriceChartContainer
             inputCurrencyId={inputCurrencyId}
             inputCurrency={currencies[Field.INPUT]}
@@ -97,7 +97,7 @@ export default function Swap() {
             isOpen={isChartDisplayed}
             setIsOpen={setIsChartDisplayed}
           />
-        )}
+        )} */}
         <Flex flexDirection="column">
           <StyledSwapContainer $isChartExpanded={isChartExpanded}>
             <StyledInputCurrencyWrapper mt={isChartExpanded ? '24px' : '0'}>
