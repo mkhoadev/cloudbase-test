f48c094f6e69e9286eb3a5edc7c77c4940948023
ntnhanbk <ntnhanbk@gmail.com>
2023-05-26 08:37:18 +0700

    fix: update code

--- mineswap-frontend/src/pages/add/[[...currency]].tsx
+++ mineswap-frontend/src/pages/add/[[...currency]].tsx
@@ -67,27 +67,26 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { currency = [] } = params
   const [currencyIdA, currencyIdB] = currency
   const match = currencyIdA?.match(OLD_PATH_STRUCTURE)
-  console.log('currency', currency)
 
-  if (match?.length) {
-    return {
-      redirect: {
-        statusCode: 301,
-        destination: `/add/${match[0]}/${match[1]}`,
-      },
-    }
-  }
+  // if (match?.length) {
+  //   return {
+  //     redirect: {
+  //       statusCode: 301,
+  //       destination: `/add/${match[0]}/${match[1]}`,
+  //     },
+  //   }
+  // }
 
-  if (currencyIdB && currencyIdB && currencyIdA.toLowerCase() === currencyIdB.toLowerCase()) {
-    return {
-      redirect: {
-        statusCode: 303,
-        destination: `/add/${currencyIdA}/${currencyIdB}`,
-      },
-    }
-  }
+  // if (currencyIdB && currencyIdB && currencyIdA.toLowerCase() === currencyIdB.toLowerCase()) {
+  //   return {
+  //     redirect: {
+  //       statusCode: 303,
+  //       destination: `/add/${currencyIdA}/${currencyIdB}`,
+  //     },
+  //   }
+  // }
 
   return {
-    props: {},
+    props: { currency },
   }
 }
