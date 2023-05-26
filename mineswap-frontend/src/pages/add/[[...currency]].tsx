94653fe11c50828c50d5aca49be175d509c756d0
ntnhanbk <ntnhanbk@gmail.com>
2023-05-26 17:02:45 +0700

    fix: page not found when reload

--- mineswap-frontend/src/pages/add/[[...currency]].tsx
+++ mineswap-frontend/src/pages/add/[[...currency]].tsx
@@ -9,30 +9,31 @@ import { useAppDispatch } from 'state'
 import { resetMintState } from 'state/mint/actions'
 import { CHAIN_IDS } from 'utils/wagmi'
 import AddLiquidity from 'views/AddLiquidity'
+import { GetServerSideProps } from 'next'
 // import AddStableLiquidity from 'views/AddLiquidity/AddStableLiquidity/index'
 // import useStableConfig, { StableConfigContext } from 'views/Swap/StableSwap/hooks/useStableConfig'
 
-export async function getServerSideProps({ params }) {
-  // Fetch data or perform any server-side logic here
-  const { currency = [] } = params
-  const dynamicParams = currency.join('/') // Combine the dynamic parameters into a single string if needed
+// export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+//   // Fetch data or perform any server-side logic here
+//   const { currency = [] }: any = params
+//   const dynamicParams = currency?.join('/') // Combine the dynamic parameters into a single string if needed
 
-  // Return the props object
-  return {
-    props: {
-      dynamicParams,
-    },
-  }
-}
+//   // Return the props object
+//   return {
+//     props: {
+//       dynamicParams,
+//     },
+//   }
+// }
 
-const AddLiquidityPage = ({ dynamicParams }) => {
+const AddLiquidityPage = ({ dynamicParams = '' }) => {
   const router = useRouter()
   const { chainId } = useActiveWeb3React()
   const dispatch = useAppDispatch()
 
   const native = useNativeCurrency()
 
-  const [currencyIdA, currencyIdB] = dynamicParams.split('/') || [
+  const [currencyIdA, currencyIdB] = dynamicParams?.split('/') || [
     native.symbol,
     CLOUD[chainId]?.address ?? USDC[chainId]?.address,
   ]
@@ -70,37 +71,38 @@ export default AddLiquidityPage
 
 // const OLD_PATH_STRUCTURE = /^(0x[a-fA-F0-9]{40}|WETH)-(0x[a-fA-F0-9]{40}|WETH)$/
 
-// export const getStaticPaths: GetStaticPaths = () => {
-//   return {
-//     paths: [{ params: { currency: [] } }],
-//     fallback: true,
-//   }
-// }
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [{ params: { currency: [] } }],
+    fallback: true,
+  }
+}
 
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-//   const { currency = [] } = params
-//   const [currencyIdA, currencyIdB] = currency
-//   const match = currencyIdA?.match(OLD_PATH_STRUCTURE)
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { currency = [] }: any = params
+  const dynamicParams = currency?.join('/') // Combine the dynamic parameters into a single string if needed
+  // const [currencyIdA, currencyIdB] = currency
+  // const match = currencyIdA?.match(OLD_PATH_STRUCTURE)
 
-//   if (match?.length) {
-//     return {
-//       redirect: {
-//         statusCode: 301,
-//         destination: `/add/${match[0]}/${match[1]}`,
-//       },
-//     }
-//   }
+  // if (match?.length) {
+  //   return {
+  //     redirect: {
+  //       statusCode: 301,
+  //       destination: `/add/${match[0]}/${match[1]}`,
+  //     },
+  //   }
+  // }
 
-//   if (currencyIdB && currencyIdB && currencyIdA.toLowerCase() === currencyIdB.toLowerCase()) {
-//     return {
-//       redirect: {
-//         statusCode: 303,
-//         destination: `/add/${currencyIdA}`,
-//       },
-//     }
-//   }
+  // if (currencyIdB && currencyIdB && currencyIdA.toLowerCase() === currencyIdB.toLowerCase()) {
+  //   return {
+  //     redirect: {
+  //       statusCode: 303,
+  //       destination: `/add/${currencyIdA}`,
+  //     },
+  //   }
+  // }
 
-//   return {
-//     props: {},
-//   }
-// }
+  return {
+    props: { dynamicParams },
+  }
+}
